c270d069de8c0cc017ef985176100d50998f2aa0
Viljami Ruokonen <vruokonen@gmail.com>
2019-04-17 17:43:23 +0300

    Added rules to start screen, and if snek is paused.

--- snek/src/snek/snekGame.java
+++ snek/src/snek/snekGame.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 public class snekGame extends Applet implements Runnable, KeyListener {
 
 	Graphics gfx;
@@ -20,10 +19,7 @@ public class snekGame extends Applet implements Runnable, KeyListener {
 	snek snek;
 	boolean gameOver;
 	Token token;
-	
-	
-		
-			
+
 	public void init() {
 		this.resize(1400, 800);
 		gameOver = false;
@@ -35,20 +31,29 @@ public class snekGame extends Applet implements Runnable, KeyListener {
 		thread = new Thread(this);
 		thread.start();
 		String TITLE = "lskflskf";
-			
+
 	}// init
-	
-		
-	
+
 	public void paint(Graphics g) {
 		gfx.setColor(Color.darkGray);
-		gfx.fillRect(0, 0, 1400, 800);	
+		gfx.fillRect(0, 0, 1400, 800);
+
+		if (!snek.isMoving) {
+			printRules();
+			/*
+			 * gfx.setColor(Color.white); gfx.setFont(new Font(Font.MONOSPACED, Font.PLAIN,
+			 * 20));// score font and font-size gfx.drawString("Ohjeet tähän:", 300, 200);
+			 * // draws string points
+			 */
+		} // if snek is not moving show game rules
+
 		if (!gameOver) {
 			snek.draw(gfx);
+			gfx.setFont(new Font(Font.MONOSPACED, Font.ITALIC, 35));// score font and font-size
 			gfx.drawString("Score:" + token.getScore(), 20, 50); // draws string points
 			token.draw(gfx);
-			gfx.setFont(new Font(Font.MONOSPACED, Font.ITALIC, 35));// score font and font-size
-		} 
+
+		}
 
 		else {
 			gfx.setColor(Color.red);
@@ -58,21 +63,15 @@ public class snekGame extends Applet implements Runnable, KeyListener {
 			gfx.setFont(new Font(Font.MONOSPACED, Font.ITALIC, 30)); // Final Score font and font-size
 			gfx.drawString("Score: " + token.getScore(), 620, 400); // Final Score and position
 		} // else
-		
+
 		g.drawImage(img, 0, 0, null); // draws image inside screen
-		
+
 	}// paint
-	
-	
 
 	public void update(Graphics g) {
 		paint(g);
-		
-				
+
 	}// update
-	
-	
-	
 
 	public void repaint(Graphics g) {
 		paint(g);
@@ -92,39 +91,29 @@ public class snekGame extends Applet implements Runnable, KeyListener {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} // catch
-			
-			
-						
-		}	
-					
-				
-		
+
+		}
+
 	}// run
-	
-	
-					
 
 	public void checkGameOver() {
 
-		if (snek.getX() < 0 || snek.getX() > 1400-20) { // if snek runs to wall in X position
+		if (snek.getX() < 0 || snek.getX() > 1400 - 20) { // if snek runs to wall in X position
 			gameOver = true;
 		} // if snek runs wall in X position
 
-		if (snek.getY() < 0 || snek.getY() > 800-20) { // if snek runs to wall in Y position
+		if (snek.getY() < 0 || snek.getY() > 800 - 20) { // if snek runs to wall in Y position
 			gameOver = true;
 		} // if snek runs wall in Y position
 
 		if (snek.snekCollision()) {
 			gameOver = true;
 		} // if
-		
-					
 
 	}// checkGameOver
 
-			
-		public void keyPressed(KeyEvent e) {
-		
+	public void keyPressed(KeyEvent e) {
+
 		if (!snek.isMoving) {
 			if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN
 					|| e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -157,23 +146,15 @@ public class snekGame extends Applet implements Runnable, KeyListener {
 			if (snek.getXDirection() != -1) { // if snek is not going down
 				snek.setXDirection(1);
 				snek.setYDirection(0); // snek can't go up or down if it is going left or right
-				
-		
-				
+
 			} // if
 		} // if
-		
-
-		
 
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-			if(snek.isMoving)
+			if (snek.isMoving)
 				snek.setIsMoving(false);
-			} // if
 		} // if
-
-
-
+	} // if
 
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -182,20 +163,27 @@ public class snekGame extends Applet implements Runnable, KeyListener {
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 	}
-	
-	public void rules() throws FileNotFoundException {
-		final Scanner scanner = new Scanner(new File("snekRules.txt"));
+
+	public void printRules() {
+
 		String line = " ";
-		
-		while (scanner.hasNext()) {
-			line = scanner.nextLine();
-			System.out.println(line);
+		int linePositionX = 300; // print position X
+		int linePositionY = 200;// print position Y
+
+		try {
+			final Scanner scanner = new Scanner(new File("snekRules.txt")); // opens snekRules.txt files
+			while (scanner.hasNext()) { // while snekRules.txt file has next line scanner reads it in
+				line = scanner.nextLine(); // scanner saves new line in line named variable
+				gfx.setColor(Color.white); // sets text color to white
+				gfx.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));// score font and font-size
+				gfx.drawString(line, linePositionX, linePositionY); // draws string what is inside the line variable
+				linePositionY += 50; // adds +50 to linePositionY, so next line comes under previous line
+			} // while
+			scanner.close();
+		} catch (Exception e) { // prints out to console error message if scanner can't find right file
+			System.out.println("Couldn't open file.");
 		}
-		
-		scanner.close();
-				
-	} 
-		
+
+	}
 
 }
-
